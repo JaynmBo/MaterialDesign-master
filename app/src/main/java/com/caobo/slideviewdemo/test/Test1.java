7a6618cd5494f4c5e5d4f55c4d3db8c32a30ba9e
Bo Cao <bo.cao@noexternalmail.hsbc.com>
2021-01-28 16:13:59 +0800

    Update Test Java log

--- app/src/main/java/com/caobo/slideviewdemo/test/Test1.java
+++ app/src/main/java/com/caobo/slideviewdemo/test/Test1.java
@@ -1,7 +1,10 @@
 package com.caobo.slideviewdemo.test;
 
+import android.util.Log;
+
 public class Test1 {
     public static void main(String[] args) {
+        Log.e("caleb","test");
 
     }
 }
--- app/src/main/java/com/caobo/slideviewdemo/test/Test2.java
+++ app/src/main/java/com/caobo/slideviewdemo/test/Test2.java
@@ -1,7 +1,9 @@
 package com.caobo.slideviewdemo.test;
 
+import android.util.Log;
+
 public class Test2 {
     public static void main(String[] args) {
-
+        Log.e("caleb","test");
     }
 }
