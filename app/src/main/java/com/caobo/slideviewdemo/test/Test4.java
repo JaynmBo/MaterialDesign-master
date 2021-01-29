1bb87af7e92b301ff213ed2e737622d9e9f07325
Bo Cao <bo.cao@noexternalmail.hsbc.com>
2021-01-29 10:45:26 +0800

    Update Log 3

--- app/src/main/java/com/caobo/slideviewdemo/test/Test4.java
+++ app/src/main/java/com/caobo/slideviewdemo/test/Test4.java
@@ -1,4 +1,11 @@
 package com.caobo.slideviewdemo.test;
 
+import androidx.annotation.NonNull;
+
 public class Test4 {
+    @NonNull
+    @Override
+    public String toString() {
+        return super.toString();
+    }
 }
