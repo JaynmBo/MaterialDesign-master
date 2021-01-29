33d9210ebdf6ffe5b438ece7e42795038ca85da2
Bo Cao <bo.cao@noexternalmail.hsbc.com>
2021-01-29 10:45:45 +0800

    Update Log 4

--- app/src/main/java/com/caobo/slideviewdemo/test/Test4.java
+++ app/src/main/java/com/caobo/slideviewdemo/test/Test4.java
@@ -2,10 +2,18 @@ package com.caobo.slideviewdemo.test;
 
 import androidx.annotation.NonNull;
 
+import com.google.gson.internal.$Gson$Preconditions;
+
 public class Test4 {
     @NonNull
     @Override
     public String toString() {
         return super.toString();
+    }
+
+    @NonNull
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
     }
 }
